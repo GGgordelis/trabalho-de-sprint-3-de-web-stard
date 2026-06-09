0687196c39cf4d7d3e488a886e2537ae9ab949ec
Zucchiniisa <>
2026-06-09 12:16:40 -0300

    alteração

---
+++ homepage.js
@@ -0,0 +1,17 @@
+document.getElementById("agendarBtn")
+.addEventListener("click", () => {
+
+    alert("Abrindo tela de agendamento");
+
+    // window.location.href = "agendamento.html";
+
+});
+
+document.getElementById("viagensBtn")
+.addEventListener("click", () => {
+
+    alert("Abrindo minhas viagens");
+
+    // window.location.href = "viagens.html";
+
+});
